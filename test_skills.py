bd9adf939cbd5052fb94ea86b40376f6e2a4a4d1
Giri priya Mummareddy <giripriyamummareddy@gmail.com>
2026-09-18 15:36:41 +0530

    Modify resume text for skill extraction test
    Updated resume text to include more skills for extraction.

--- test_skills.py
+++ test_skills.py
@@ -1,14 +1,14 @@
 from utils.skill_extractor import extract_skills
 
 
-# Sample resume text
+
 resume_text = """
 I have experience in Python, Java, AWS, Linux,
 PySpark, SQL, Docker and Generative AI.
 """
 
 
-# Extract skills
+
 skills = extract_skills(resume_text)
 
 
